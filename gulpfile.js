e4f6c85166ef3f213cd5770a9acceff8f7523a91
Ben Plate <bplate9583@gmail.com>
2022-08-20 13:09:26 -0700

    update pwa caching

--- gulpfile.js
+++ gulpfile.js
@@ -6,19 +6,73 @@ gulp.task("hugo-build", shell.task(["hugo --minify"])); // this can change depen
 
 gulp.task("generate-service-worker", () => {
   return workbox.generateSW({
-    cacheId: "yourCacheID",
+    cacheId: "ben9583-pwa-cache",
     globDirectory: "./public", // the directory where static file is stored
     swDest: "./public/sw.js", // destination of sw.js file
-    globPatterns: ["**/*.{js,css,html}"],
+    globPatterns: ["**/*.{js,css,html,png,jpg,jpeg,gif,bmp,svg,ico,mp3,mp4,eot,ttf,woff,json,xml}"], // glob patterns to match files to cache
     navigateFallback: "/404.html", // fallback file
     runtimeCaching: [
       {
-        urlPattern: /\.(?:png|jpg|jpeg|gif|bmp|webp|svg|ico)\/?$/,
-        handler: "CacheFirst",
+        urlPattern: /\.(?:html|js)?$/,
+        handler: "NetworkFirst",
+        options: {
+          cacheName: "html-js",
+          expiration: {
+            maxEntries: 100,
+            maxAgeSeconds: 31 * 24 * 60 * 60 // 1 month
+          }
+        }
       },
       {
-        urlPattern: /\.(?:json|xml)\/?$/,
+        urlPattern: /\.(?:css)?$/,
+        handler: "StaleWhileRevalidate",
+        options: {
+          cacheName: "css",
+          expiration: {
+            maxEntries: 100,
+            maxAgeSeconds: 31 * 24 * 60 * 60 // 1 month
+          }
+        }
+      },
+      {
+        urlPattern: /\.(?:png|jpg|jpeg|gif|bmp|webp|svg|ico)?$/,
+        handler: "CacheFirst",
+        options: {
+          cacheName: "images",
+          expiration: {
+            maxEntries: 20,
+            maxAgeSeconds: 7 * 24 * 60 * 60, // one week
+          }
+        }
+      },
+      {
+        urlPattern: /\.(?:mp3|mp4)?$/,
+        handler: "CacheFirst",
+        options: {
+          cacheName: "media",
+          expiration: {
+            maxEntries: 20,
+            maxAgeSeconds: 7 * 24 * 60 * 60, // one week
+          }
+        }
+      },
+      {
+        urlPattern: /\.(eot|ttf|woff)?$/,
+        handler: "CacheFirst",
+        options: {
+          cacheName: "fonts",
+          expiration: {
+            maxEntries: 20,
+            maxAgeSeconds: 31 * 24 * 60 * 60, // one month
+          }
+        }
+      },
+      {
+        urlPattern: /\.(?:json|xml)?$/,
         handler: "NetworkOnly",
+        options: {
+          cacheName: "json-xml",
+        }
       },
     ],
   });
