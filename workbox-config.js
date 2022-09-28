033bacb83c54ca8a2d61504e44bdabdbd545beb9
Ben Plate <bplate9583@gmail.com>
2022-09-28 09:26:49 -0700

    remove html caching

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	globDirectory: 'public/',
 	globPatterns: [
-		'**/*.{html,png,jpg,jpeg,gif,css,ico,ttf,svg,webp,js,mp3,txt}'
+		'**/*.{png,jpg,jpeg,gif,css,ico,ttf,svg,webp,js,mp3,txt}'
 	],
 	swDest: 'public/sw.js',
 	ignoreURLParametersMatching: [
